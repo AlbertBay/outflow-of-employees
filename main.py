5d49fe5206c7ff7f73c51fd6bd1789cec52c077f
Albert Bay <albertbay@MacBook-Air-Albert.local>
2022-10-09 22:04:33 +0300

    от меира

--- main.py
+++ main.py
@@ -1,152 +1,65 @@
-import numpy as np
+import os
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.metrics import roc_auc_score, classification_report, roc_curve
-from sklearn.preprocessing import OneHotEncoder, StandardScaler, OrdinalEncoder
-from sklearn.compose import ColumnTransformer
-from sklearn.model_selection import GridSearchCV
+
+from time import time
+
+from constants import DATA_FOLDER_NAME, DATAFRAMES, MODELS, PREDICT_FOLDER_NAME, VISUALISATION_FOLDER_NAME
+from renew import Pipe, splitter
 
 
-class Preprocessing:
-    """
-    Класс для препроцессинга, заполняет пропуски и бинаризует колонки с большим колличеством пропусков
-    нормализует данные для линейных методов
+def model_run(df_name: str, x_train: pd.DataFrame, x_test: pd.DataFrame, y_train: pd.Series, y_test: pd.Series,
+              model: dict, storage_path: str, visualisation_path: str):
     """
 
-    def __init__(self, cat: list, num: list, is_linear: bool, fill_func=np.mean):
-        """
-
-        :param cat: категориальные признаки
-        :param num: вещественные признаки
-        :param is_linear: флаг, True для линейных методов
-        :param fill_func: агрегирующая функция np, для заполнения пропусков в лин методах
-        """
-
-        self.cat = cat
-        self.num = num
-        self.is_linear = is_linear
-        self.to_binarize = None
-
-        self.fill_func = fill_func
-        self.fill_values = None
-        if is_linear:
-            self.column_transformer = ColumnTransformer([
-                ('ohe', OneHotEncoder(handle_unknown="ignore"), self.cat),
-                ('scaling', StandardScaler(), self.num)
-            ])
-        else:
-            self.column_transformer = ColumnTransformer([
-                ('ordinal', OrdinalEncoder(handle_unknown='use_encoded_value',
-                                           unknown_value=-1), self.cat),
-                ('chill', 'passthrough', self.num)
-
-            ])
-
-    def fit_transform(self, df: pd.DataFrame):
-        """
-
-        :param df: датафрейм для обработки
-        :return: догадайся
-        """
-
-        # бинаризуем скудные признаки
-        self.to_binarize = np.array(df.columns)[(df.isna().sum() / df.shape[0] > 0.5).values]
-        df[self.to_binarize] = df[self.to_binarize].isna().astype(int)
-        # заполняем пропуски для вещественных признаков агрегатами, или -1000 для бустинга
-        if self.is_linear:
-            self.fill_values = dict(self.fill_func(df[self.num]))
-            df[self.num] = df[self.num].fillna(value=self.fill_values)
-        else:
-            df[self.num] = df[self.num].fillna(value=-1000)
-        # заполняем оставщееся пропуски
-        df[self.cat] = df[self.cat].fillna(-1000)
-        # OHE или Ordinal encoder
-        df = self.column_transformer.fit_transform(df)
-        return df
-
-    def transform(self, df: pd.DataFrame):
-        """
-        Догадайся
-        :param df: ты
-        :return: можешь...
-        """
-        df[self.to_binarize] = df[self.to_binarize].isna().astype(int)
-        df[self.cat] = df[self.cat].fillna(-1)
-        if self.is_linear:
-            df[self.num] = df[self.num].fillna(value=self.fill_values)
-        else:
-            df[self.num] = df[self.num].fillna(value=-1000)
-        df = self.column_transformer.transform(df)
-        return df
-
-
-class Pipe:
+    @param df_name:
+    @param x_train:
+    @param x_test:
+    @param y_train:
+    @param y_test:
+    @param model:
+    @param storage_path:
+    @param visualisation_path:
     """
-    Класс для прогона моделей
+    pipe = Pipe(df_name,
+                model['estimator_name'],
+                model['model'],
+                model['is_linear'],
+                storage_path,
+                visualisation_path)
+    cat = x_train.select_dtypes(['object']).columns.tolist()
+    num = x_train.select_dtypes(['int64', 'float64']).columns.tolist()
+    pipe.fit(x_train, y_train, model['params'], cat, num)
+    pipe.predict(x_test, y_test)
+
+
+def run(data_folder: str = DATA_FOLDER_NAME, predict_folder: str = PREDICT_FOLDER_NAME,
+        visualisation_folder: str = VISUALISATION_FOLDER_NAME):
     """
 
-    def __init__(self, model, is_linear: bool):
-        """
+    :param data_folder:
+    :param predict_folder:
+    :param visualisation_folder:
+    """
+    path = os.getcwd()
+    data_path = path + '/' + data_folder
+    predict_path = path + '/' + predict_folder
+    visualisation_path = path + '/' + visualisation_folder
 
-        :param model: модель поддерживающая fit и predict наследованный от склерновской базы
-        :param is_linear: флаг - линейный алгоритм или нет
-        """
-        self.model = model
-        self.final_model = None
-        self.prep = None
-        self.is_linear = is_linear
+    for filename in os.listdir(data_path):
+        if filename.endswith('.csv'):
+            print(f'Started working with {filename[:-4]} dataset.')
+            df = pd.read_csv(data_path + '/' + filename)
+            x_train, x_test, y_train, y_test = splitter(df,
+                                                        DATAFRAMES[filename]['features'],
+                                                        DATAFRAMES[filename]['y'])
+            for model in MODELS:
+                model_run(filename[:-4], x_train, x_test, y_train, y_test, model, predict_path, visualisation_path)
 
-    def fit(self, x_train, y_train, params: dict, cat: list, num: list, cv: int = 5):
-        """
+            print(f'Ended working with {filename[:-4]} dataset. Switching to next.')
 
-        :param x_train:
-        :param y_train:
-        :param params: параметры для перебора гридсерчем
-        :param cat: категориальные признаки
-        :param num: вещественные признаки
-        :param cv: колличество фолдов для кросс валидации
-        :return: лучшая модель
-        """
-        self.prep = Preprocessing(cat=cat, num=num, is_linear=self.is_linear)
-        x_train = self.prep.fit_transform(x_train)
-        self.final_model = GridSearchCV(self.model,
-                                        params,
-                                        cv=cv,
-                                        scoring='roc_auc',
-                                        return_train_score=False,
-                                        verbose=1,
-                                        n_jobs=-1)
-        self.final_model.fit(x_train, y_train)
-        print('-----------------------------------------------')
-        print(f'best_params is {self.final_model.best_params_}')
-        print(f'best_score is {self.final_model.best_score_}')
-        print()
-        return self.final_model.best_estimator_
 
-    def inference(self, x_test, y_test):
-        x_test = self.prep.transform(x_test)
-        y_pred_proba = self.final_model.predict_proba(x_test)[:, 1]
-        y_pred_class = self.final_model.predict(x_test)
-        print('-----------------------------------------------')
-        print(f'ROC_AUC on test - {roc_auc_score(y_test, y_pred_proba)}')
-        print(classification_report(y_test, y_pred_class, target_names=['class 0', 'class 1']))
-
-        false_positive_rate, true_positive_rate, thresolds = roc_curve(y_test, y_pred_proba)
-        plt.figure(figsize=(10, 8), dpi=100)
-        plt.axis('scaled')
-        plt.xlim([0, 1])
-        plt.ylim([0, 1])
-        plt.title("AUC & ROC Curve")
-        plt.plot(false_positive_rate, true_positive_rate, 'g')
-        plt.fill_between(false_positive_rate, true_positive_rate, facecolor='lightgreen', alpha=0.7)
-        #plt.text(0.95, 0.05, ha='right', fontsize=12, weight='bold', color='blue')
-        plt.xlabel("False Positive Rate")
-        plt.ylabel("True Positive Rate")
-        plt.show()
-
-        figure, axis = plt.subplots(1, 2, figsize=(10, 15))
-        axis[0].hist(y_pred_proba[y_test == 1])
-        axis[0].set_title('1 CLASS')
-        axis[1].hist(y_pred_proba[y_test == 0])
-        axis[1].set_title('0 CLASS')
-        return
+if __name__ == '__main__':
+    st_time = time()
+    run()
+    print(f'Total time spent: {(time() - st_time):.3f}s')
+    print('Finish!')
---
+++ renew.py
@@ -0,0 +1,181 @@
+import joblib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from sklearn.base import BaseEstimator
+from sklearn.compose import ColumnTransformer
+from sklearn.metrics import roc_auc_score, classification_report, roc_curve
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, OrdinalEncoder
+from typing import Callable, Optional
+
+from constants import TEST_SIZE
+
+
+def splitter(df: pd.DataFrame, features: list, y: str) -> tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    """
+
+    @return:
+    """
+    x_train, x_test, y_train, y_test = train_test_split(df[features], df[y], TEST_SIZE)
+    return x_train, x_test, y_train, y_test
+
+
+class Preprocessing:
+    """
+
+    """
+    cat: list
+    column_transformer: ColumnTransformer
+    fill_func: Callable
+    fill_values: dict
+    is_linear: bool
+    num: list
+    to_binarize: Optional[np.array]
+
+    def __init__(self, cat: list, num: list, is_linear: bool, fill_func: Callable = np.mean):
+        """
+
+        @param cat:
+        @param num:
+        @param is_linear:
+        @param fill_func:
+        """
+        self.cat = cat
+        self.num = num
+        self.is_linear = is_linear
+        self.fill_func = fill_func
+        self.to_binarize = None
+
+        if is_linear:
+            self.column_transformer = ColumnTransformer([
+                ('ohe', OneHotEncoder(handle_unknown="ignore"), self.cat),
+                ('scaling', StandardScaler(), self.num)
+            ])
+        else:
+            self.column_transformer = ColumnTransformer([
+                ('ordinal', OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1), self.cat),
+                ('chill', 'passthrough', self.num)
+            ])
+
+    def fit_transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+
+        @param df:
+        @return:
+        """
+        self.to_binarize = np.array(df.columns)[(df.isna().sum() / df.shape[0] > 0.5).values]
+        df[self.to_binarize] = df[self.to_binarize].isna().astype(int)
+        if self.is_linear:
+            self.fill_values = dict(self.fill_func(df[self.num]))
+            df[self.num] = df[self.num].fillna(value=self.fill_values)
+        else:
+            df[self.num] = df[self.num].fillna(value=-1000)
+        df[self.cat] = df[self.cat].fillna(-1000)
+        df = self.column_transformer.fit_transform(df)
+        return df
+
+    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+
+        @param df:
+        @return:
+        """
+        df[self.to_binarize] = df[self.to_binarize].isna().astype(int)
+        df[self.cat] = df[self.cat].fillna(-1)
+        if self.is_linear:
+            df[self.num] = df[self.num].fillna(value=self.fill_values)
+        else:
+            df[self.num] = df[self.num].fillna(value=-1000)
+        df = self.column_transformer.transform(df)
+        return df
+
+
+class Pipe:
+    """
+
+    """
+    dataframe_name: str
+    estimator_name: str
+    final_model: Optional[BaseEstimator]
+    is_linear: bool
+    model: BaseEstimator
+    prep: Optional[Preprocessing]
+    storage_path: str
+    visualisation_path: str
+
+    def __init__(self, dataframe_name: str, estimator_name: str, model: BaseEstimator, is_linear: bool,
+                 storage_path: str, visualisation_path: str):
+        """
+
+        @param estimator_name:
+        @param model:
+        @param is_linear:
+        @param storage_path:
+        """
+        self.dataframe_name = dataframe_name
+        self.estimator_name = estimator_name
+        self.model = model
+        self.is_linear = is_linear
+        self.storage_path = storage_path
+        self.visualisation_path = visualisation_path
+
+    def fit(self, x_train: pd.DataFrame, y_train: pd.Series, params: dict, cat: list, num: list, cv: int = 5):
+        """
+
+        @param x_train:
+        @param y_train:
+        @param params:
+        @param cat:
+        @param num:
+        @param cv:
+        """
+        self.prep = Preprocessing(cat=cat, num=num, is_linear=self.is_linear)
+        x_train = self.prep.fit_transform(x_train)
+        final_model = GridSearchCV(self.model,
+                                   params,
+                                   cv=cv,
+                                   scoring='roc_auc',
+                                   return_train_score=False,
+                                   verbose=1,
+                                   n_jobs=-1)
+        final_model.fit(x_train, y_train)
+        print('-----------------------------------------------------------------------------------------------------')
+        print(f'best_params for {self.estimator_name} is {final_model.best_params_}')
+        print(f'best_score for {self.estimator_name} is {final_model.best_score_}')
+        print('-----------------------------------------------------------------------------------------------------\n')
+        self.final_model = final_model.best_estimator_
+        joblib.dump(self.final_model, self.storage_path + f'/{self.dataframe_name}_{self.estimator_name}_model.pkl')
+
+    def predict(self, x_test: pd.DataFrame, y_test: pd.Series):
+        """
+
+        @param x_test:
+        @param y_test:
+        """
+        x_test = self.prep.transform(x_test)
+        y_pred_proba = self.final_model.predict_proba(x_test)[:, 1]
+        y_pred_class = self.final_model.predict(x_test)
+        print('-----------------------------------------------')
+        print(f'ROC_AUC on test - {roc_auc_score(y_test, y_pred_proba)}')
+        print(classification_report(y_test, y_pred_class, target_names=['class 0', 'class 1']))
+
+        false_positive_rate, true_positive_rate, thresholds = roc_curve(y_test, y_pred_proba)
+        plt.figure(figsize=(10, 8), dpi=100)
+        plt.axis('scaled')
+        plt.xlim([0, 1])
+        plt.ylim([0, 1])
+        plt.title("AUC & ROC Curve")
+        plt.plot(false_positive_rate, true_positive_rate, 'g')
+        plt.fill_between(false_positive_rate, true_positive_rate, facecolor='lightgreen', alpha=0.7)
+        plt.xlabel("False Positive Rate")
+        plt.ylabel("True Positive Rate")
+        plt.savefig(self.visualisation_path + f'/{self.dataframe_name}_{self.estimator_name}_ROC_AUC.jpg')
+
+        figure, axis = plt.subplots(1, 2, figsize=(10, 15))
+        axis[0].hist(y_pred_proba[y_test == 1])
+        axis[0].set_title('1 CLASS')
+        axis[1].hist(y_pred_proba[y_test == 0])
+        axis[1].set_title('0 CLASS')
+        plt.savefig(self.visualisation_path + f'/{self.dataframe_name}_{self.estimator_name}_hist.jpg')
